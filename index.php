df9b87132b92f68ce8bc4430f74ef35e495c4d63
Lucas <vintersr@gmail.com>
2019-08-15 11:13:18 -0600

    arquivos da aula hoje

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php
+
+// Basically, what it does is, selects the right swipe button class name, and keeps clicking
+
+func = setInterval(function() {
+ var swype  = document.getElementsByClassName("recsGamepad__button--like")
+  swype[0].click()
+}, 1000)
+?>
