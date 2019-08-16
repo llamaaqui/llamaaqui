da9bd65962f9d2a6b5510e97b7f7266922700075
Gustavo <vergiliopoleza@gmail.com>
2019-08-16 09:07:59 -0300

    Atualização cadastro

--- Cadastro.php
+++ Cadastro.php
@@ -2,88 +2,91 @@
 <html>
 <head>
 	<title>Cadastro</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="modelo.css">
 </head>
 <body>
 
-	<div id="formulario">
-		<form method="post">
+<div id="formulario_cadastro">
+	<form method="post">
 
-			<label for="nome">Nome Completo</label><br>
-			<input type="text" name="nome" id="nome" required="">
+		<label for="nome">Nome Completo</label><br>
+		<input class="input_form" type="text" name="nome" id="nome" required="">
 
-			<br><br>
+		<br><br>
 
-			<label for="email">E-mail</label><br>
-			<input type="email" name="email" id="email" required="">
+		<label for="email">E-mail</label><br>
+		<input class="input_form" type="email" name="email" id="email" required="">
 
-			<br><br>
+		<br><br>
 
-			<label for="senha">Senha</label><br>
-			<input type="password" name="senha" id="senha" required="">
+		<label for="senha">Senha</label><br>
+		<input class="input_form" type="password" name="senha" id="senha" required="">
 
-			<br><br>
+		<br><br>
 
-			<label for="senha2">Repita a senha</label><br>
-			<input type="password" name="senha2" id="senha2" required="">
+		<label for="senha2">Repita a senha</label><br>
+		<input class="input_form" type="password" name="senha2" id="senha2" required="">
 
-			<br><br>
+		<br><br>
 
-			<input type="radio" name="sexo" id="masculino" required="">
-			<label for="masculino">Masculino</label>
-			<input type="radio" name="sexo" id="feminino">
-			<label for="feminino">Feminino</label>
+		<input class="input_radio" type="radio" name="sexo" id="masculino" required="">
+		<label for="masculino">Masculino</label><br>
 
-			<br><br>
+		<input class="input_radio" type="radio" name="sexo" id="feminino">
+		<label for="feminino">Feminino</label>
 
-			<label for="data_nascimento">Data de Nascimento</label><br>
-			<select class="select">
+		<br><br>
 
-				<option>Dia</option>
-				<?php for ($i = 1; $i <= 31; $i++) {?>
-				<option><?=$i?></option>
-				<?php } ?>
+		<label for="data_nascimento">Data de Nascimento</label><br>
+		<select class="select" required="">
 
-			</select>
+			<option>Dia</option>
+			<?php for ($i = 1; $i <= 31; $i++) {?>
+			<option><?=$i?></option>
+			<?php } ?>
 
-
-			<select class="select">
-				<option>Mês</option>
-				<option>Janeiro</option>
-				<option>Fevereiro</option>
-				<option>Março</option>
-				<option>Abril</option>
-				<option>Maio</option>
-				<option>Junho</option>
-				<option>Julho</option>
-				<option>Agosto</option>
-				<option>Setembro</option>
-				<option>Outubro</option>
-				<option>Novembro</option>
-				<option>Dezembro</option>
-			</select>
+		</select>
 
 
-			<select class="select">
+		<select class="select" required="">
+			<option>Mês</option>
+			<option>Janeiro</option>
+			<option>Fevereiro</option>
+			<option>Março</option>
+			<option>Abril</option>
+			<option>Maio</option>
+			<option>Junho</option>
+			<option>Julho</option>
+			<option>Agosto</option>
+			<option>Setembro</option>
+			<option>Outubro</option>
+			<option>Novembro</option>
+			<option>Dezembro</option>
+		</select>
 
-				<option>Ano</option>
-				<?php for ($i = date('Y'); $i >= (date('Y') - 100); $i--) {?>
-				<option><?=$i?></option>
-				<?php } ?>
 
-			</select>
+		<select class="select" required="">
 
-			<br><br>
+			<option>Ano</option>
+			<?php for ($i = date('Y'); $i >= (date('Y') - 100); $i--) {?>
+			<option><?=$i?></option>
+			<?php } ?>
 
-			<label>Número de Telefone</label><br>
-			<input type="number" name="telefone" id="telefone" placeholder="(xx) 9 xxxx-xxxx">
+		</select>
 
-			<br><br>
+		<br><br>
 
-			<label>Número de celular</label><br>
-			<input type="number" name="celular" id="celular" placeholder="(xx) 9 xxxx-xxxx">
+		<label>Número de Telefone</label><br>
+		<input class="input_form" type="number" name="telefone" id="telefone" placeholder="Ex: 00 9 0000 0000">
 
-		</form>
-	</div>
+		<br><br>
+
+		<label>Número de celular</label><br>
+		<input class="input_form" type="number" name="celular" id="celular" placeholder="Ex: 00 9 0000 0000">
+
+	</form>
+</div>
 
 </body>
 </html>
