0485b247e272e1bcfb18a088d535cfcf5861bc33
Gustavo <vergiliopoleza@gmail.com>
2019-08-16 11:48:21 -0300

    Cadastro Atualizado

--- Cadastro.php
+++ Cadastro.php
@@ -10,38 +10,44 @@
 
 	<?php require_once "includes/navbar.php"; ?>
 
-	<div id="formulario_cadastro">
-		<form method="post">
 
-			<label for="nome">Nome Completo</label><br>
-			<input class="input_form" type="text" name="nome" id="nome" required="">
+	<div class="formulario_cadastro">
+		<form class="form-group" method="post">
 
-			<br><br>
-
-			<label for="email">E-mail</label><br>
-			<input class="input_form" type="email" name="email" id="email" required="">
+			<h1 id="titulo_cadastro">Criar uma conta</h1>
+			
+			<input class="input_form" type="text" name="nome" id="nome" required="" placeholder="Nome">
 
 			<br><br>
 
-			<label for="senha">Senha</label><br>
-			<input class="input_form" type="password" name="senha" id="senha" required="">
+			<input class="input_form" type="email" name="email" id="email" required="" placeholder="E-mail">
 
 			<br><br>
 
-			<label for="senha2">Repita a senha</label><br>
-			<input class="input_form" type="password" name="senha2" id="senha2" required="">
+			<input class="input_form" type="password" name="senha" id="senha" required="" placeholder="Senha">
 
 			<br><br>
 
-			<input class="input_radio" type="radio" name="sexo" id="masculino" required="">
-			<label for="masculino">Masculino</label><br>
-
-			<input class="input_radio" type="radio" name="sexo" id="feminino">
-			<label for="feminino">Feminino</label>
+			<input class="input_form" type="password" name="senha2" id="senha2" required="" placeholder="Repetir a senha">
 
 			<br><br>
 
-			<label for="data_nascimento">Data de Nascimento</label><br>
+			<p class="p_cadastro">Sexo</p>	
+
+			<div style="text-align: center">
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="masculino" name="sexo" class="custom-control-input" required="">
+					<label class="custom-control-label" for="masculino">Masculino</label>
+				</div>
+				<div class="custom-control custom-radio custom-control-inline">
+					<input type="radio" id="feminino" name="sexo" class="custom-control-input">
+					<label class="custom-control-label" for="feminino">Feminino</label>
+				</div>
+			</div>
+
+			<br>
+
+
 			<select class="select" required="">
 
 				<option>Dia</option>
@@ -80,13 +86,14 @@
 
 			<br><br>
 
-			<label>Número de Telefone</label><br>
-			<input class="input_form" type="number" name="telefone" id="telefone" placeholder="Ex: 00 9 0000 0000">
+			<input class="input_form" type="number" name="telefone" id="telefone" placeholder="Número de Telefone">
 
 			<br><br>
 
-			<label>Número de celular</label><br>
-			<input class="input_form" type="number" name="celular" id="celular" placeholder="Ex: 00 9 0000 0000">
+			<input class="input_form" type="number" name="celular" id="celular" placeholder="Número de celular">
+
+
+			<button class="btn btn-success btn-block mt-5" type="submit">Enviar</button>
 
 		</form>
 	</div>
