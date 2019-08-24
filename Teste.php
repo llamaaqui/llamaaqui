f3cf44c4cb55465ffca9b6300f1bef7089e686a9
Vergiliogu <54104457+Vergiliogu@users.noreply.github.com>
2019-08-23 22:52:06 -0300

    Muitas alterações OH

--- Teste.php
+++ Teste.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
+	<title>Teste</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css_e_js/padrao.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
